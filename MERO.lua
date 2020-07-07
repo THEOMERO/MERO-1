77da50a4cfd48c7d15126ac926018a96ddfc406c
MEROBOOT <64145412+MEROBOOT@users.noreply.github.com>
2020-07-07 15:54:31 +0300

    Add files via upload

--- MERO.lua
+++ MERO.lua
@@ -147,8 +147,8 @@ print([[
 |/ \___/ (_______)   \_/   |_/    \/|/     \|
 
 > CH › @MERO170
-> CH › @pvv_v
-~> DEVELOPER › @KKKKK4
+> CH › @MERO_01
+~> DEVELOPER › @FEEEM
 ]])
 sudos = dofile("./Info.lua") 
 SUDO = tonumber(sudos.SUDO)
@@ -157,11 +157,8 @@ bot_id = sudos.token:match("(%d+)")
 token = sudos.token 
 --- start functions ↓
 --------------------------------------------------------------------------------------------------------------
-io.popen("mkdir File_Bot") 
-io.popen("cd File_Bot && rm -rf commands.lua.1") 
-io.popen("cd File_Bot && rm -rf commands.lua.2") 
-io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/MEROTEAM/Files_MERO/master/File_Bot/commands.lua") 
+iio.popen("mkdir File_Bot") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/MEROBOOT/Files_MERO/master/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
