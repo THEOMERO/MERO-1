27cc9ea9fc23c420db7dcba762444f8628714393
MEROBOOT <64145412+MEROBOOT@users.noreply.github.com>
2020-07-07 16:15:07 +0300

    Add files via upload

--- MERO.lua
+++ MERO.lua
@@ -654,12 +654,12 @@ return false
 end
 end,nil)   
 end  
-function plugin_Poyka(msg)
+function plugin_mero(msg)
 for v in io.popen('ls File_Bot'):lines() do
 if v:match(".lua$") then
 plugin = dofile("File_Bot/"..v)
-if plugin.Poyka and msg then
-pre_msg = plugin.Poyka(msg)
+if plugin.mero and msg then
+pre_msg = plugin.mero(msg)
 end
 end
 end
@@ -9535,7 +9535,7 @@ end
 end   
 --------------------------------------------------------------------------------------------------------------
 SourceMERO(data.message_,data)
-plugin_Poyka(data.message_)
+plugin_mero(data.message_)
 --------------------------------------------------------------------------------------------------------------
 if Chat_Type == 'GroupBot' and ChekAdd(msg.chat_id_) == true then
 tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,data) 
